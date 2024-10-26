05ada9e89d996425c5c313825326be97ff9b3823
Ricardo <ricardobassanesi@gmail.com>
2024-10-25 22:33:21 -0300

    insert mongo

--- 12_mongodb/02_mvc/controllers/ProductController.mjs
+++ 12_mongodb/02_mvc/controllers/ProductController.mjs
@@ -1,7 +1,23 @@
 import Product from "../models/Product.mjs";
 
 export default class ProductController {
-  static showProduct = (req, res) => {
+  static showProducts = (req, res) => {
     res.render("products/all");
+  };
+
+  static createProduct = (req, res) => {
+    res.render("products/create");
+  };
+
+  static createProductPost = (req, res) => {
+    const name = req.body.name;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    const product = new Product(name, price, description);
+
+    product.save();
+
+    res.redirect("/products");
   };
 }
