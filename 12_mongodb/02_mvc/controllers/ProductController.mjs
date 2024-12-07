51e8743fa188e6fefd6ee77cb708280e80c5b7f0
Ricardo <ricardobassanesi@gmail.com>
2024-12-07 19:59:52 -0300

    fim novo mvc

--- 12_mongodb/02_mvc/controllers/ProductController.mjs
+++ 12_mongodb/02_mvc/controllers/ProductController.mjs
@@ -1,7 +1,28 @@
-// import Product from "../models/Product.mjs";
+import Product from "../models/Product.mjs";
 
 export default class ProductController {
+  //static
+  //Static class methods are defined on the class itself.
+
+  //You cannot call a static method on an object, only on an object class.
+
   static showProducts = (req, res) => {
     res.render("products/all");
+  };
+
+  static createProduct = (req, res) => {
+    res.render("products/create");
+  };
+
+  static createProductPost = (req, res) => {
+    const name = req.body.name;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    const product = new Product(name, price, description);
+
+    product.save();
+
+    res.redirect("/products");
   };
 }
--- 12_mongodb/02_mvc/routes/productsRoutes.mjs
+++ 12_mongodb/02_mvc/routes/productsRoutes.mjs
@@ -3,6 +3,8 @@ import ProductController from "../controllers/ProductController.mjs";
 
 const router = express.Router();
 
+router.get("/create", ProductController.createProduct);
+router.post("/create", ProductController.createProductPost);
 router.get("/", ProductController.showProducts);
 
 export default router;
