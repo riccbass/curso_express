29e79b42fb2fd6d3324f3ed99ed8f484229e6011
Ricardo <ricardobassanesi@gmail.com>
2024-12-14 13:30:03 -0300

    fim api!

--- 14_api/01_primeira_api/index.mjs
+++ 14_api/01_primeira_api/index.mjs
@@ -12,8 +12,20 @@ app.use(express.json());
 
 //rotas - endpoints
 
+app.post("/create_product", (req, res) => {
+  const { name, price } = req.body;
+
+  if (!name) {
+    res.status(422).json({ message: "Usuário não informado" });
+  }
+
+  console.log(price);
+
+  res.status(201).json({ message: `Olá mundo ${name}` });
+});
+
 app.get("/", (req, res) => {
-  res.json({ message: "Olá mundo" });
+  res.status(200).json({ message: "Olá mundo" });
 });
 
 app.listen(3000);
