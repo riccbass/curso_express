445b5790acef070eb0832f326bd7bc1aa5a4377e
Ricardo <ricardobassanesi@gmail.com>
2024-12-14 22:55:34 -0300

    avanço pelo jwt

--- 15_get_a_pet/backend/controllers/UserController.mjs
+++ 15_get_a_pet/backend/controllers/UserController.mjs
@@ -1,6 +1,8 @@
 import createUserToken from "../helpers/create-user-token.mjs";
+import getToken from "../helpers/get-token.mjs";
 import User from "../models/User.mjs";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export default class UserController {
   static register = async (req, res) => {
@@ -27,6 +29,7 @@ export default class UserController {
       res.status(422).json({ message: "A senha é obrigatória" });
       return;
     }
+
     if (!confirmpassword) {
       res.status(422).json({ message: "A confirmação da senha é obrigatória" });
       return;
@@ -68,5 +71,55 @@ export default class UserController {
     } catch (error) {
       res.status(500).json({ message: error });
     }
+  };
+
+  static login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+      res.status(422).json({ message: "O e-mail é obrigatório" });
+      return;
+    }
+
+    if (!password) {
+      res.status(422).json({ message: "A senha é obrigatória" });
+      return;
+    }
+
+    //check if user exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(422).json({ message: "E-mail não cadastrado" });
+      return;
+    }
+
+    //check if password match
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+      res.status(422).json({ message: "Senha incorreta" });
+      return;
+    }
+
+    await createUserToken(user, req, res);
+  };
+
+  static checkUser = async (req, res) => {
+    let currentUser;
+
+    console.log(req.headers.authorization);
+
+    if (req.headers.authorization) {
+      const token = getToken(req);
+      const decoded = jwt.verify(token, "juventue1913secret");
+
+      currentUser = await User.findById(decoded.id);
+      currentUser.password = undefined;
+    } else {
+      currentUser = null;
+    }
+
+    res.status(200).send(currentUser);
   };
 }
--- 15_get_a_pet/backend/routes/UserRoutes.mjs
+++ 15_get_a_pet/backend/routes/UserRoutes.mjs
@@ -4,5 +4,7 @@ import UserController from "../controllers/UserController.mjs";
 const router = express.Router();
 
 router.post("/register", UserController.register);
+router.post("/login", UserController.login);
+router.get("/checkuser", UserController.checkUser);
 
 export default router;
